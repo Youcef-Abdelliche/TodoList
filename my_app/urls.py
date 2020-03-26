0e9931a2688b2d71686657870bdd4f717830e94b
Youcef-Abdelliche <hy_abdelliche@esi.dz>
2020-03-26 07:00:55 +0100

    add delete function

--- my_app/urls.py
+++ my_app/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('add_todo/', views.add_todo, name='new_item')
+    path('add_todo/', views.add_todo, name='new_item'),
+    path('delete/<int:item_id>/', views.delete_todo),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from .models import TodoList
 from django.http import HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
 
 
 def home(request):
@@ -9,9 +10,14 @@ def home(request):
     return render(request, 'index.html', {'items_list': items_list})
 
 
+@csrf_exempt
 def add_todo(request):
     item_text = request.POST.get('add_item')
     TodoList.objects.create(text=item_text)
     return HttpResponseRedirect('/')
 
 
+@csrf_exempt
+def delete_todo(request, item_id):
+    TodoList.objects.filter(id=item_id).delete()
+    return HttpResponseRedirect('/')
